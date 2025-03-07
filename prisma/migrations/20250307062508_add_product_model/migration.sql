b7e2779e80701a40f0847e30a11fe69d164e2b35
Ali Baloch <62090625+balochali@users.noreply.github.com>
2025-03-07 11:25:37 +0500

    Added Product Model

---
+++ prisma/migrations/20250307062508_add_product_model/migration.sql
@@ -0,0 +1,15 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "role" "Role" NOT NULL DEFAULT 'USER';
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "price" DOUBLE PRECISION NOT NULL,
+    "stock" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
